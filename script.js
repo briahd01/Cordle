43b5e4e438f3697758facfa2bbc293c3b04e0f7c
briahd01 <56639924+briahd01@users.noreply.github.com>
2022-11-11 12:55:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-// created by Genesis Clarke & Briah Davis 
-// CS340 Project -- Cordle 
-
 'use strict';
 
 // create a file stream
 //var fs = require('fs');
 // constant variable - number of guesses will remain the same
 const NUM_GUESSES = 6;
+
 // create variable to count the guesses remaining, store the user's guess and the computer's chosen word to guess
 let guessRemaining = NUM_GUESSES;
-let userGuess = "";
-let computerWord = "";
-let nextLetter = 0;
-let score = 0;
+let userGuess = ""; // will store users input
+let computerWord = ""; // store random word
+let nextLetter = 0; // for inserting and deleting
 
 // exception handling for opening a file
     try {
       // read in file
-      // var wordList = fs.readFileSync('c:/Users/Briah/OneDrive/Documents/JavaScriptPL/words.txt').toString().split("\r\n");
-      // short list for demo purposes
+      //var wordList = fs.readFileSync('c:/Users/Briah/OneDrive/Documents/JavaScriptPL/words.txt').toString().split("\r\n");
+
       var wordList = ["world", "april", "thief", "march", "clone"]
       var index = Math.floor(Math.random() * wordList.length);
+
       // find and store a random word from list
 	    computerWord = wordList[index];
     }
@@ -33,15 +31,17 @@ let score = 0;
 function gameBoard(){
   let board = document.getElementById("game-board");
 
+  // create 6 row elements
   for (let i = 0; i < NUM_GUESSES; ++i){
     let row = document.createElement("div");
     row.className = "letter-row";
-
+    // create 5 elements for each row (will become the boxes)
     for (let j = 0; j < 5; ++j){
       let box = document.createElement("div");
       box.className = "letter-box";
       row.appendChild(box);
     }
+    // add each row to the board
     board.appendChild(row);
   }
 }
@@ -53,6 +53,7 @@ document.addEventListener("keyup", (e) =>{
   }
   // convert pressed key letter/word into a string
   let pressedKey = String(e.key);
+
   // if backspace, delete the letter
   if (pressedKey == "Backspace"){
     deleteLetter();
@@ -64,7 +65,7 @@ document.addEventListener("keyup", (e) =>{
    return;
   }
 
-  let found = pressedKey.match(/[a-z]/gi)
+  let found = pressedKey.match(/[a-z]/gi);
   // check if the key is a letter
   if (!found || found.length > 1){
     return;
@@ -76,71 +77,86 @@ document.addEventListener("keyup", (e) =>{
   }
 })
 
+// removes the letter from the box
 function deleteLetter(){
+  // find the row the letter is in
   let index = NUM_GUESSES - guessRemaining;
   let row = document.getElementsByClassName("letter-row")[index];
 
+  // find the box that has the letter
   let square = row.children[nextLetter - 1];
+  // remove the letter
   square.textContent = "";
-  square.classList.remove("filled-box");
+  square.classList.remove("filled-box"); // remove boarder
+  
+  // now the user guess has one less letter
   userGuess = userGuess.slice(0, userGuess.length - 1);
   nextLetter--;
 
 }
 
+// add the letter to the box
 function insertLetter(keyPressed){
+  // ignore if full
   if (nextLetter == 5){
     return;
   }
+  // make it lowercase to properly compare
   keyPressed = keyPressed.toLowerCase();
 
+  // find row
   let index = NUM_GUESSES - guessRemaining;
   let row = document.getElementsByClassName("letter-row")[index];
 
+  // find square
   let square = row.children[nextLetter];
   square.textContent = keyPressed;
   square.classList.add("filled-box");
-  userGuess += keyPressed;
+  userGuess += keyPressed; // add the letter to the word
   nextLetter++;
 }
 
 // compare the guess with the expected word from word list
 function compare(){
+  // find row
   let index = NUM_GUESSES - guessRemaining;
   let row = document.getElementsByClassName("letter-row")[index];
 
+  // if the word is not five letters long, send an error
   if (userGuess.length != 5){
     alert("ERROR: Not enough letters!");
     return;
 
   }
+  // if the word is not in the list, send an error
   if (!wordList.includes(userGuess)){
-    alert("ERROR: Word is not in list!");
+    alert("ERROR: Word is not in list");
     return;
   }
 
     var i = 0;
-    var color = "";
+    // loop through each character of the word
     while (i < 5){
+      // if they are in the same position, color the box indigo
       if (userGuess[i] == computerWord[i]){
         let square = row.children[i];
         square.style.backgroundColor = "indigo";
-        color = "indigo";
   
       }
+      // if the letter is not in the computers word, color it gray
       else if (!computerWord.includes(userGuess[i])) {
         let square = row.children[i];
         square.style.backgroundColor = "gray";
-        color = "gray";
       }
+      // if the letter is in the word, color it yello
       else {
         let square = row.children[i];
         square.style.backgroundColor = "yellow";
-        color = "yellow";
       }
+      // move to next letter
       i++;
     }
-
+    // if the user one, send a message and reset
     if (userGuess == computerWord){
       alert("You got it!");
       guessRemaining = 0;
@@ -151,31 +167,13 @@ function compare(){
       guessRemaining--;
       userGuess = "";
       nextLetter = 0;
+      // if the user lost, send an error
       if (guessRemaining == 0){
         alert("Out of tries! The word was " + computerWord);
       }
     }
   }
 
-
+  // create game board
   gameBoard();
 
-  // // reassign everything 
-  // function newGame(){
-  //   userGuess = "";
-  //   computerWord = "";
-  //   nextLetter = 0;
-  //   // exception handling for opening a file
-  //   try {
-  //     // read in file
-  //     //var wordList = fs.readFileSync('c:/Users/Briah/OneDrive/Documents/JavaScriptPL/words.txt').toString().split("\r\n");
-  //     var wordList = ["world", "april", "these", "march", "clone"]
-  //     var index = Math.floor(Math.random() * wordList.length);
-  //     // find and store a random word from list
-	//     computerWord = wordList[index];
-  //   }
-  //   catch (err){
-  //     console.log(err);
-  //   }
-  //   gameBoard();
-  // }
